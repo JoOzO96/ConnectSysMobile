47bd480a85e9016d4c7232cf9118c602e1a2c3cd
José Alcides Benetti de Souza <141865@upf.br>
2019-09-09 16:15:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/connectsys/uteis/Mascara.java
@@ -0,0 +1,145 @@
+package com.example.connectsys.uteis;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+/**
+ * Created by Jose on 25/05/2017.
+ */
+
+public class Mascara {
+    public static String unmask(String s) {
+        return s.replaceAll("[.]", "").replaceAll("[-]", "")
+                .replaceAll("[/]", "").replaceAll("[(]", "")
+                .replaceAll("[)]", "");
+    }
+
+    public static TextWatcher insert(final String mask, final EditText ediTxt) {
+        return new TextWatcher() {
+            boolean isUpdating;
+            String old = "";
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = Mascara.unmask(s.toString());
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
+                        mascara += m;
+                        continue;
+                    }
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                ediTxt.setText(mascara);
+                ediTxt.setSelection(mascara.length());
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+    }
+
+    public static TextWatcher cpfcnpj(final EditText ediTxt) {
+        return new TextWatcher() {
+            String mask = "";
+            boolean isUpdating;
+            String old = "";
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (ediTxt.length() <= 14) {
+                    mask = "###.###.###-##";
+                } else {
+                    mask = "##.###.###/####-##";
+                }
+                String str = Mascara.unmask(s.toString());
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
+                        mascara += m;
+                        continue;
+                    }
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                ediTxt.setText(mascara);
+                ediTxt.setSelection(mascara.length());
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+    }
+
+    public static TextWatcher datas(final EditText ediTxt) {
+        return new TextWatcher() {
+            String mask = "";
+            boolean isUpdating;
+            String old = "";
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                mask = "##/##/####";
+
+                String str = Mascara.unmask(s.toString());
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
+                        mascara += m;
+                        continue;
+                    }
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                ediTxt.setText(mascara);
+                ediTxt.setSelection(mascara.length());
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+    }
+}
